3224ad738f8ba3a06a7a6e1e2516f97377089628
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-13 23:05:04 +0530

    feat(main) : get data from session

--- HibernateApp/src/main/java/launchApp/LaunchMainApp.java
+++ HibernateApp/src/main/java/launchApp/LaunchMainApp.java
@@ -26,22 +26,30 @@ public class LaunchMainApp {
 			sessionFactory = configuration.buildSessionFactory();
 			session = sessionFactory.openSession();
 			transaction = session.beginTransaction();
-			
-			Employee emp = new Employee();
-			emp.setEno(101);
-			emp.setEname("Anjali");
-			emp.setEsal(3000);
-			emp.setEaddr("HYD");
-			session.persist(emp);   // session.save(); but save is deprecated
-		//	session.update(emp);  // for updation
-		//	session.delete(emp);
-			transaction.commit();
-			System.out.println("Eplmloyee Updated Successful");
-			System.out.println("Transaction success");
+//			
+//			Employee emp = new Employee();
+//			emp.setEno(101);
+//			emp.setEname("Anjali");
+//			emp.setEsal(3000);
+//			emp.setEaddr("HYD");
+//			session.persist(emp);   // session.save(); but save is deprecated
+//		//	session.update(emp);  // for updation
+//		//	session.delete(emp);
+//			transaction.commit();
+//			System.out.println("Eplmloyee Updated Successful");
+//			System.out.println("Transaction success");
+//			
+			Employee emp = (Employee) session.get("entity.Employee", 101);
+			System.out.println("Employee details");
+			System.out.println("-----------------");
+			System.out.println("Employee Number  : "+emp.getEno());
+			System.out.println("Employee Name    : "+emp.getEname());
+			System.out.println("Employee Salary  : "+emp.getEsal());
+			System.out.println("Employee Address : "+emp.getEaddr());
 			
 		} catch (Exception e) {
-			transaction.rollback();
-			System.out.println("Transaction failure");
+//			transaction.rollback();
+//			System.out.println("Transaction failure");
 			e.printStackTrace();
 		}
 		finally {
