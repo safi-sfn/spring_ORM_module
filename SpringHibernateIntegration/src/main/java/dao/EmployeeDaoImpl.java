1745e1d3c2dc90b2562ed7c52f8a1caa387b83b3
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-15 21:27:42 +0530

    feat(dao) : Remaining operation (search,update,delete) completed by using hibernateTemplte

--- SpringHibernateIntegration/src/main/java/dao/EmployeeDaoImpl.java
+++ SpringHibernateIntegration/src/main/java/dao/EmployeeDaoImpl.java
@@ -20,8 +20,13 @@ public class EmployeeDaoImpl implements IEmployeeDao {
    @Transactional
 	@Override
 	public String insert(Employee emp) {
+	   Employee emp1 = null;
 		String status = "";
 		try {
+			emp1 = (Employee)hibernateTemplate.get(Employee.class, emp.getEno());
+			if(emp1 != null) {
+				System.out.println("Employee already existed");
+			}else {
 			int eno = (Integer)hibernateTemplate.save(emp);
 			if(eno == emp.getEno()) {
 				status = "Employee inserted  successfull";
@@ -29,6 +34,7 @@ public class EmployeeDaoImpl implements IEmployeeDao {
 			else {
 				status = "Employe insertion failure";
 			}
+		   }
 		} catch (Exception e) {
 			status = "Employe insertion failure";
 			e.printStackTrace();
@@ -36,22 +42,60 @@ public class EmployeeDaoImpl implements IEmployeeDao {
 		return status;
 	}
 
+   @Transactional
 	@Override
 	public Employee search(int eno) {
-	
-		return null;
+		Employee emp = null;
+		try {
+			emp = (Employee)hibernateTemplate.get(Employee.class, eno);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return emp;
 	}
 
+	@Transactional
 	@Override
 	public String update(Employee emp) {
-
-		return null;
+		String status = "";
+		try {
+			Employee emp1 = search(emp.getEno());
+			if(emp1 ==null) {
+					status = "Employee Not Existed";
+			}
+			else {
+				hibernateTemplate.evict(emp1);
+				hibernateTemplate.update(emp);
+				status = "Employee Updation success";
+			}
+		} catch (Exception e) {
+			status="Employee updation failure";
+			e.printStackTrace();
+		}
+		
+		return status;
 	}
 
+	@Transactional
 	@Override
 	public String delete(Employee emp) {
+		String status = "";
+		try {
+			Employee emp1 = search(emp.getEno());
+			if(emp1 ==null) {
+					status = "Employee Not Existed";
+			}
+			else {
+				hibernateTemplate.evict(emp1);
+				hibernateTemplate.delete(emp);
+				status = "Employee Deleted success";
+			}
+		} catch (Exception e) {
+			status="Employee deletion failure";
+			e.printStackTrace();
+		}
 		
-		return null;
+		return status;
 	}
 
 }
