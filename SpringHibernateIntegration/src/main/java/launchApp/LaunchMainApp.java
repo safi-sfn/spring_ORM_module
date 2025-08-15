794bf708d99d3507594b45878253524f0ef39d4e
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-15 21:28:02 +0530

    feat(main): all done

--- SpringHibernateIntegration/src/main/java/launchApp/LaunchMainApp.java
+++ SpringHibernateIntegration/src/main/java/launchApp/LaunchMainApp.java
@@ -11,16 +11,47 @@ public class LaunchMainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		IEmployeeDao empDao = (IEmployeeDao) context.getBean("empDao");
-		
+
+/*		
+//	Insert operation	
 		Employee emp = new Employee();
-		emp.setEno(104);
+		emp.setEno(106);
 		emp.setEname("Mohit");
 		emp.setEsal(4000);
 		emp.setEaddr("NYC");
 		
 		String status = empDao.insert(emp);
 		System.out.println(status);
-
+*/
+/*
+		Employee emp = empDao.search(101);
+		if(emp == null) {
+			System.out.println("Employee not Existed");
+		}else {
+			System.out.println("Employee Details");
+			System.out.println("---------------------");
+			System.out.println("Employee Name     : "+emp.getEname());
+			System.out.println("Employee Sallary  : "+emp.getEsal());
+			System.out.println("Employee Address  : "+emp.getEaddr());
+		}
+*/
+	/*
+		Employee emp = new Employee();
+		emp.setEno(106);
+		emp.setEname("Mohit");
+		emp.setEsal(3000);
+		emp.setEaddr("NYC");
+		String status = empDao.update(emp);
+		System.out.println(status);
+	*/
+		
+		Employee emp = new Employee();
+		emp.setEno(106);
+		emp.setEname("Mohit");
+		emp.setEsal(3000);
+		emp.setEaddr("NYC");
+		String status = empDao.delete(emp);
+		System.out.println(status);
 	}
 
 }
